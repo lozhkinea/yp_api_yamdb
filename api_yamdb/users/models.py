f9ba8ae68c9f1ddc4824e637698fd27710cbf82b
Эдуард Ложкин <lozhkinea@ya.ru>
2022-05-12 05:02:32 +0300

    fix role field

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,7 +4,14 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = [
+        "user",
+        "moderator",
+        "admin",
+    ]
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     email = models.EmailField(_("email address"), unique=True, blank=True)
     bio = models.TextField(_("biography"), blank=True)
-    role = models.CharField(_("user role"), max_length=150)
+    role = models.CharField(
+        _("user role"), max_length=9, choiced=ROLE_CHOICES, default="user"
+    )
