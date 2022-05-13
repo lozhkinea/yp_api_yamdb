7b4674e2428d57141c7d70886a9c08ee291b112d
Tatyana Kim <kimtv279@gmail.com>
2022-05-13 08:48:19 +0600

    исправление моделей и добавление urls.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,49 +2,41 @@ from django.db import models
 
 
 # id,name,slug
-class Categories(models.Model):
+class Category(models.Model):
     # Поля базы данных
     # CharField - строка с ограничением длины
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=256)
     # SlugField - помогает создавать дружелюбные URL-адреса
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 # id,name,slug
-class Genres(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
 
 
 # id,name,year,category
-class Titles(models.Model):
-    name = models.CharField(max_length=200)
+class Title(models.Model):
+    name = models.CharField(max_length=256)
     # IntegerField - для хранения целых чисел
     # установит значение поля в NULL, т.е. нет данных
-    year = models.IntegerField(null=True)
+    year = models.IntegerField()
     # ??? непонятно
     # rating = models.ManyToManyField(Review)
     # TextField - для больших текстовых блоков
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, null=True)
     # ForeignKey, ссылка на модель Categories
     # в колонке category будут указаны pk записей из таблицы Categories
-    genre = models.ForeignKey(
-        Genres,
-        blank=True,
-        null=True,
-        # если удаляется жанр, произведения остаются
-        on_delete=models.SET_NULL,
-        # связь между моделями
-        related_name='titles'
-    )
+    genre = models.ManyToManyField(Genre)
     category = models.ForeignKey(
-        Categories,
+        Category,
         blank=True,
         null=True,
         # если удаляется категория, произведения остаются
