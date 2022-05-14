0f0d9d757837fae40bd3dbfe175ceebaf2f87fdd
Mashabor <96130611+Mashabor@users.noreply.github.com>
2022-05-14 20:11:33 +0300

    models Post and Comment (#6)
    * models Post and Comment
    
    * Change_1 models Review and Comment
    
    * Change_2 models Review and Comment
    
    * Change_3 models Review and Comment
    
    * Change_4 models Review and Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,7 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+from users.models import User
 
 
 class Category(models.Model):
@@ -29,3 +32,42 @@ class Title(models.Model):
     def __str__(self):
         return (f"name={self.name[:15]}, "
                f"year={self.year}")
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1, 'Оценка должна быть не ниже 1'),
+            MaxValueValidator(10, 'Оценка должна быть не выше 10')
+        ]
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
