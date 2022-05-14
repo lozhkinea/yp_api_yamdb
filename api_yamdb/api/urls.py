8a2b38354f05130edfc1e4ec2dd8c5024561870b
Eduard <96784094+lozhkinea@users.noreply.github.com>
2022-05-14 10:36:59 +0300

    first migrations (#5)

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,9 +8,9 @@ app_name = "api"
 
 router_v1 = SimpleRouter()
 router_v1.register("users", views.UserViewSet, basename="users")
-router_v1.register("titles", views.TitleViewSet, basename="titles")
-router_v1.register("genres", views.GenreViewSet, basename="genres")
-router_v1.register("categories", views.CategoryViewSet, basename="categories")
+# router_v1.register("titles", views.TitleViewSet, basename="titles")
+# router_v1.register("genres", views.GenreViewSet, basename="genres")
+# router_v1.register("categories", views.CategoryViewSet, basename="categories")
 
 urlpatterns = [
     path("v1/", include(router_v1.urls)),
@@ -19,5 +19,4 @@ urlpatterns = [
         TokenObtainPairView.as_view(),
         name="token",
     ),
-    path("", include(router_v1.urls)),
 ]
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,16 +2,20 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+USER = "USR"
+MODERATOR = "MDR"
+ADMIN = "ADM"
+ROLE_CHOICES = [
+    (USER, "user"),
+    (MODERATOR, "moderator"),
+    (ADMIN, "admin"),
+]
+
 
 class User(AbstractUser):
-    ROLE_CHOICES = [
-        "user",
-        "moderator",
-        "admin",
-    ]
     first_name = models.CharField(_("first name"), max_length=150, blank=True)
     email = models.EmailField(_("email address"), unique=True, blank=True)
     bio = models.TextField(_("biography"), blank=True)
     role = models.CharField(
-        _("user role"), max_length=9, choiced=ROLE_CHOICES, default="user"
+        _("user role"), max_length=9, choices=ROLE_CHOICES, default="user"
     )
