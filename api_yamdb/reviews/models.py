2e36c899e8220529bc3c15cf27ef1a4901e6b12d
Mariia Borshcheva <mariiaborshcheva@yandex.ru>
2022-05-14 18:49:11 +0300

    Change_4 models Review and Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from users.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
