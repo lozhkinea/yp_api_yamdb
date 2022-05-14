d8e33efd513745f178b1b7f4ffa69570d009d0ce
Mariia Borshcheva <mariiaborshcheva@yandex.ru>
2022-05-14 09:43:37 +0300

    models Post and Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -29,3 +29,44 @@ class Title(models.Model):
     def __str__(self):
         return (f"name={self.name[:15]}, "
                f"year={self.year}")
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    image = models.ImageField(
+        upload_to='posts/',
+        null=True,
+        blank=True
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='groups',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField()
+    created = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True)
