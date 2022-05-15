9ee83164e54bf048adc11b1de8ae635f69cfdc57
Mariia Borshcheva <mariiaborshcheva@yandex.ru>
2022-05-15 12:29:29 +0300

    First serializers for review and comment

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+
 from users.models import User
+from reviews.models import Review, Comment
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,3 +15,37 @@ class UserSerializer(serializers.ModelSerializer):
             "bio",
             "role",
         )
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    title = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+    def validate(self, data):
+        request = self.context.get('request')
+        if request.method == 'POST':
+            title_id = self.context['view'].kwargs.get('title_id'),
+            author = self.context['request'].user
+            if Review.objects.filter(title=title_id, author=author).exists():
+                raise serializers.ValidationError(
+                    'Вы уже оставили отзыв на этот обзор.'
+                )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
