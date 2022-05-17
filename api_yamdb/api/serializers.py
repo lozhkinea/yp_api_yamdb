184bc3be5a211c98ae8a974d18c79396254856d7
Eduard <96784094+lozhkinea@users.noreply.github.com>
2022-05-17 17:56:52 +0300

    Feature/users/srlzr (#8)
    * 1
    
    * 2
    
    * 3
    
    * 4
    
    * auth first version
    
    * tests passed
    
    * fix readme

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -13,3 +13,26 @@ class UserSerializer(serializers.ModelSerializer):
             "bio",
             "role",
         )
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+        )
+
+    def validate(self, data):
+        if data["username"] == "me":
+            raise serializers.ValidationError('Cannot create user "me"')
+        return data
+
+
+class UserTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "confirmation_code",
+        )
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 from api import views
 
@@ -14,9 +13,6 @@ router_v1.register("users", views.UserViewSet, basename="users")
 
 urlpatterns = [
     path("v1/", include(router_v1.urls)),
-    path(
-        "auth/token/",
-        TokenObtainPairView.as_view(),
-        name="token",
-    ),
+    path("v1/auth/signup/", views.signup, name="signup"),
+    path("v1/auth/token/", views.token, name="token"),
 ]
