1857dd7350960f2d2a0de69926c45e9f1b4850ce
Mashabor <96130611+Mashabor@users.noreply.github.com>
2022-05-21 21:39:36 +0300

    Constraints added, migrations made, permissions changed (#26)

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -66,8 +66,13 @@ class ReviewAndComment(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.user.role in ['moderator', 'admin']
-            or request.user.is_superuser
-            or obj.author == request.user
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or request.user
+            and request.user.is_authenticated
+            and (
+                request.user.role in ['moderator', 'admin']
+                or request.user.is_superuser
+                or obj.author == request.user
+            )
         )
